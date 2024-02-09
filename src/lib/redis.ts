3dc7696b1e5cecfd97af5974e9b5dde758b9c0e0
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-02-08 22:14:48 -0300

    :wrench: chore: redis config

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,4 +1,15 @@
 import { Redis } from "ioredis";
 
-export const redis = new Redis();
+const redisOptions =
+  process.env.REDIS_PORT !== "6379"
+    ? {
+        port: Number(process.env.REDIS_PORT!), // Redis port
+        host: process.env.REDIS_HOSTNAME!, // Redis host
+        username: "default", // needs Redis >= 6
+        password: process.env.REDIS_PASSWORD!,
+        db: 0, // Defaults to 0
+      }
+    : {};
+
+export const redis = new Redis(redisOptions);
 //todo quando for publicar tem que passar as configurações aqui
